54d6145d271033361ce93c0df53836d4ec8bc8a7
Webinkk <mason.webb1997@icloud.com>
2025-03-13 13:42:39 -0700

    Completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNum = -1;
+        while (userNum != 0)
+        {
+            Console.Write("Enter a number (enter 0 to quit): ");
+
+            string userInput = Console.ReadLine();
+            userNum = int.Parse(userInput);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        }
+
+        //Part 1 - Sum
+        int numSum = 0;
+
+        foreach (int number in numbers)
+        {
+            numSum += number;
+        }
+
+        Console.WriteLine($"The sum of the numbers on the list are: {numSum}");
+
+        //Part 2 - Average (add the numbers together numSum then devide by number of values)
+        float average = ((float)numSum) / numbers.Count;
+        Console.WriteLine($"The average of this list is {average}");
+
+        //Part 3 - Find the max or largest in list
+        int maximum = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        }
+        Console.WriteLine($"The max is: {maximum}");
     }
 }
