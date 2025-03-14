57d42b949abb68beef317f6cbe39f6fcac113c8c
Webinkk <mason.webb1997@icloud.com>
2025-03-13 21:20:15 -0700

    Completed exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeMessage();
+
+        string userName = GetUserName();
+        int userNumber = GetNumber();
+        int squaredResult = SquaredNumber(userNumber);
+
+        Result(userName, squaredResult);
     }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int GetNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int squared = number * number;
+        return squared;
+    }
+
+    static void Result(string name, int squared)
+    {
+        Console.WriteLine($"{name}, the squre of your number is {squared}");
+    }
+
 }
